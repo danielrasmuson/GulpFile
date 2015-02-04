6f2c2e4dbe330b6728ff8a8d54e33975a67c61ba
Daniel Rasmuson <dan123911@gmail.com>
2015-02-04 13:07:06 -0600

    added fileinclude to my gulp build system

--- gulp/tasks/markup.js
+++ gulp/tasks/markup.js
@@ -1,9 +1,14 @@
 var gulp = require('gulp');
 var config = require('../config').markup
 var browserSync  = require('browser-sync');
+var fileinclude = require('gulp-file-include');
 
 gulp.task('markup', function() {
   return gulp.src(config.src)
+    .pipe(fileinclude({
+        prefix: '@@',
+        basepath: '@file'
+    }))
     .pipe(gulp.dest(config.dest))
     .pipe(browserSync.reload({stream:true}));
 });
