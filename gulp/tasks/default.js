6e743b345ff4c639d0788f16be67de3b4a4913d6
Daniel Rasmuson <dan123911@gmail.com>
2015-03-24 16:16:53 -0500

    added a fix to run typescript on default

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -1,3 +1,3 @@
 var gulp = require('gulp');
 
-gulp.task('default', ['bower', 'sass', 'images', 'markup', 'watch']);
+gulp.task('default', ['bower', 'sass', 'images', 'markup', 'scripts', 'watch']);
